058897cc1f6061034fa46f677713b529d24a1b15
n0nia <denisaboriceanu63@gmail.com>
2020-05-13 13:02:55 +0300

    Added Configuration class

---
+++ Configuration.py
@@ -0,0 +1,14 @@
+class Configuration:
+    '''
+    The game configuration
+
+    Attribute:
+    (int) board_width - board's width
+    (int) board_height - board's height
+    (int) number_of_mines - number of mines for the game
+    '''
+    def __init__(self, board_width, board_height, number_of_mines):
+        self.board_width = board_width
+        self.board_height = board_height
+        self.number_of_mines = number_of_mines
+
