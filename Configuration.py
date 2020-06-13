2540378eb02d1db89623ae290bd8c4aaac9c982a
Ruxandra Matei <ruxandramatei0@gmail.com>
2020-06-14 00:09:55 +0300

    start game added

--- Configuration.py
+++ Configuration.py
@@ -7,7 +7,7 @@ class Configuration:
     (int) board_height - board's height
     (int) number_of_mines - number of mines for the game
     '''
-    def __init__(self, board_width, board_height, number_of_mines):
+    def __init__(self, board_width=8, board_height=8, number_of_mines=10):
         self.board_width = board_width
         self.board_height = board_height
         self.number_of_mines = number_of_mines
