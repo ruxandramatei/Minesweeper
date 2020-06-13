c273c96a3b4a75cdbbf2e578b82f2acf6c40523d
Ruxandra Matei <ruxandramatei0@gmail.com>
2020-06-13 17:19:45 +0300

    Added run method

--- visualize.py
+++ visualize.py
@@ -30,3 +30,72 @@ class PyGameVisualizer(Visualizer):
     BOMB_TILE = 11
     FLAG_TILE = 12
     WINDOW_NAME = 'Minesweeper'
+
+    def run(self, runner):
+        game = runner.game
+
+        self.table_width = game.board_width
+        self.table_height = game.board_height
+
+        pygame.init()
+        pygame.mixer.quit()  # if we don't turn off sound, uses 100% cpu
+
+        pygame.display.set_caption(self.WINDOW_NAME)
+        
+        # give the dimensions in pixels of the screen
+        screen_pixels_width = self.TILE_SIZE * self.table_width
+        screen_pixels_height = self.TILE_SIZE * self.table_height
+
+        # create the screen
+        self.screen = pygame.display.set_mode((screen_pixels_width, screen_pixels_height))
+        self.screen.fill(self.COLOR)
+
+        # load the tiles
+        self.game_tiles = self._load_tiles()
+
+        next(runner) # get the status
+        self._draw(game) # update the screen
+
+
+        if isinstance(self.pause, str):
+
+            print("Move")
+
+            pygame.event.clear()
+
+            while not game.game_over:
+
+                event = pygame.event.wait()
+
+                if event.type == pygame.locals.KEYDOWN:
+
+                    next(runner) # get the status
+                    self._draw(game) # update the board
+
+                elif event.type == pygame.locals.QUIT:
+
+                    game.quit()
+                    break
+        else:
+
+            while not game.game_over:
+
+                time.sleep(self.pause) # wait for a move
+
+                next(runner) # get the status
+
+                self._draw(game) # update the board
+
+        if self.next_game_prompt: # game paused
+
+            print("Hit any key to continue...")
+
+            while True: # render
+
+                event = pygame.event.wait() # wait for an event
+
+                if event.type in [pygame.locals.KEYDOWN, pygame.locals.QUIT]: # resolve that event
+
+                    break
+
+        pygame.quit() # quit the game
